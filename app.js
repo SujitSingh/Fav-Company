17d1e2a7d694e2b7a123d9e29ab8490fa5e5ab89
Suji Kumar Singh <sujit98322@gmail.com>
2019-04-14 12:53:36 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('Hello expres app');
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, function(err, req, res){
+  if(err) {
+    return console.log('Error occurred while starting server');
+  }
+  console.log(`Server running at http://127.0.0.1:${port}`);
+});
