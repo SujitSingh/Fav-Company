3909a2835097b4da73c8c940d07d02c1f21c6551
Sujit Kumar Singh <sujit98322@gmail.com>
2020-06-29 23:38:39 +0530

    Upgrading to Angular 8

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'user', loadChildren: './user-checkin/user-checkin.module#UserCheckinModule' },
+  { path: 'user', loadChildren: () => import('./user-checkin/user-checkin.module').then(m => m.UserCheckinModule) },
   { path: '**', redirectTo: 'user' }
 ];
 
