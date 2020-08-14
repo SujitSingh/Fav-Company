aa52f70ff8979f2b069f9d184b46fa14ddd23599
Sujit Kumar Singh <sujit98322@gmail.com>
2020-08-14 15:42:24 +0530

    Adding environment variable and DB connections

--- app.js
+++ app.js
@@ -4,4 +4,28 @@ const session = require('express-session');
 
 const app = express();
 
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}`,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  },
+  success => {
+    console.log('Database connected');
+  },
+  error => {
+    console.log('Failed to connect database')
+  }
+);
+
+// initialize express-session
+app.use(session({
+  key: 'ss_id',
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 60 * 10 * 1000, // 10 mins
+  }
+}));
+
 module.exports = app;
